713334401eaf46366f1bc6b9e5c2792e518b9e39
yuriy <sitecadilac@mail.ru>
2024-09-17 22:04:26 +0300

    test-3

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,14 +1,10 @@
-import pytest
 import os
 from api import PetFriends
 from settings import valid_email, valid_password
 
 pf = PetFriends()
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 1de82a5 (test-1)
 def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
     """Проверяем, что запрос api ключа возвращает статус 200 и в результате содержится слово 'key'."""
     status, result = pf.get_api_key(email, password)
